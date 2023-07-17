53bd186296710b21556c131b2769b03ad17df8c5
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-07-16 22:06:30 -0600

    Added morgan

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const express = require('express');
-const { log } = require('console');
+// const { log } = require('console');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -18,6 +23,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -110,6 +117,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) ROUTES
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -117,6 +126,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// START SERVER
 
 const port = 3000;
 app.listen(port, () => {
