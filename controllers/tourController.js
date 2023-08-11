6b6d286751b61e9630dea61d490216acd1360141
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-08-10 22:32:52 -0600

    Fixed error, and handling of error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -42,7 +42,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: "fail",
-      message: err,
+      message: "Invalid data sent!",
     });
     console.error("There was an error: ", err);
   }
