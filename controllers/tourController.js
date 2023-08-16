1f54f3fc9102a08790d6818b015bcd5190fe17fd
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-08-15 22:21:45 -0600

    Added advanced filter for query string

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,23 +2,30 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = await Tour.find(queryObj);
+    // { difficulty: 'easy', duration: {$gte: 5} }
+    // gte, gt, lte, lt
+
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const query = await Tour.find()
     //   .where("duration")
     //   .equals(5)
     //   .where("difficulty")
     //   .equals("easy");
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     // SEND RESPONSE
     res.status(200).json({
