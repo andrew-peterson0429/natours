98d16fdd693187b01a61a63f30f9974bd2cf0871
andrew-peterson0429 <71342671+andrew-peterson0429@users.noreply.github.com>
2023-08-20 17:29:12 -0600

    Refactored query functionality into class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,59 +8,120 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
+class APIfeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this; // this is the entire object
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy); // this is the troubled sort method TypeError: The comparison function must be either a function or undefined
+      // sort('price ratingsAverage')
+    } else {
+      // default sort of createdAt property in desceding order
+      this.query = this.query.sort("-createdAt");
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      // When the fields property is not specified:
+      this.query = this.query.select("-__v");
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit; // The number here is all the results that come before the page we want now. Want page 3, we skip all of page 2 results.
+
+    // page=2&limit=10, 1-10 are page 1, 11-20 are page 2, etc.
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log("this is req.query: ", req.query);
     // BUILD QUERY
     // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
+    // const queryObj = { ...req.query };
+    // const excludedFields = ["page", "sort", "limit", "fields"];
+    // excludedFields.forEach((el) => delete queryObj[el]);
 
     // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // let queryStr = JSON.stringify(queryObj);
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     // { difficulty: 'easy', duration: {$gte: 5} } this is queryStr
     // gte, gt, lte, lt These are filter operators
 
     // The error was due to await, it is not returning the query but the object, .select can only be operated on query not the object, so the solution is to remove await
-    let query = Tour.find(JSON.parse(queryStr));
+    // let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy); // this is the troubled sort method TypeError: The comparison function must be either a function or undefined
-      // sort('price ratingsAverage')
-    } else {
-      // default sort of createdAt property in desceding order
-      query = query.sort("-createdAt");
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy); // this is the troubled sort method TypeError: The comparison function must be either a function or undefined
+    //   // sort('price ratingsAverage')
+    // } else {
+    //   // default sort of createdAt property in desceding order
+    //   query = query.sort("-createdAt");
+    // }
 
     // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      // When the fields property is not specified:
-      query = query.select("-__v");
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(",").join(" ");
+    //   query = query.select(fields);
+    // } else {
+    //   // When the fields property is not specified:
+    //   query = query.select("-__v");
+    // }
 
     // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit; // The number here is all the results that come before the page we want now. Want page 3, we skip all of page 2 results.
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit; // The number here is all the results that come before the page we want now. Want page 3, we skip all of page 2 results.
 
-    // page=2&limit=10, 1-10 are page 1, 11-20 are page 2, etc.
-    query = query.skip(skip).limit(limit);
+    // // page=2&limit=10, 1-10 are page 1, 11-20 are page 2, etc.
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("This page does not exist");
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error("This page does not exist");
+    // }
 
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIfeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     // SEND RESPONSE
     res.status(200).json({
