3a0a76aecdbaa0c0945cfeaea48f3adbb02ebfe1
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-08-23 22:02:01 -0600

    New route for pipeline calc, started pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -206,3 +206,36 @@ exports.getTourStats = async (req, res) => {
     console.error("There was an error: ", err);
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1; // is 2021
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+    console.error("There was an error: ", err);
+  }
+};
