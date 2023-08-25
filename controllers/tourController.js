efcfd9c4a7cef40262ffe9a0e2d1154365d9ca84
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-08-24 23:00:12 -0600

    Completed aggreagate pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -223,6 +223,27 @@ exports.getMonthlyPlan = async (req, res) => {
           },
         },
       },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 6,
+      },
     ]);
 
     res.status(200).json({
