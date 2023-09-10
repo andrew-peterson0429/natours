4da40badf9efd17425fda8c67d61cc9902227ee6
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-09-09 23:27:43 -0600

    Added some err handling for prod and dev

--- utils/appError.js
+++ utils/appError.js
@@ -5,7 +5,7 @@ class AppError extends Error {
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
     // unexpected errors might happen in our app, but will not have isOperational, so that is why we add it. Ex: a programming err or a bug in package.
-    this.isOperational = true; // This is so we can test with the property and send errors back for operational errors
+    this.isOperational = true; // This is so we can test with the property and send errors back for operational errors (all errors we create ourselves). These are the errors we want to send to client in prod
 
     Error.captureStackTrace(this, this.constructor);
   }
