694a1685cc36445ba8b58840b87a789505e31195
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-09-12 23:08:39 -0600

    Handled validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,6 +11,13 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -42,6 +49,7 @@ const sendErrorProd = (err, res) => {
 
 module.exports = (err, req, res, next) => {
   // console.log("This is the err.stack: ", err.stack);
+  let error = { ...err };
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -49,11 +57,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
+    // let error = { ...err };
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
 
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
