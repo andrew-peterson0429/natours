49064f4180b2dbe47d8f83d2fd5e3650d6a0f3f8
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-09-13 22:49:13 -0600

    Handled unhandled rejections

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose
   .then(() => console.log("db connection successful"));
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UHANDLED REJECTION! Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
 });
