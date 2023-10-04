24ea7ab6cf2e8d87fe432a40786f29b959f76c85
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-10-03 21:39:14 -0600

    instanced pass reset token method, route, midware

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
@@ -40,6 +41,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.pre("save", async function (next) {
@@ -80,6 +83,22 @@ userSchema.methods.changedPasswordAfter = function (JWTtimestamp) {
 
   // False means password has not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  console.log("This is reset token as an object: ", { resetToken });
+  console.log("This is this.passwordResetToken: ", this.passwordResetToken);
+
+  return resetToken;
 };
 
 const User = mongoose.model("User", userSchema);
