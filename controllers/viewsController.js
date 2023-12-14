4fb714f83d6edbbdbae6dda590eb4c174c396274
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2023-12-13 21:57:46 -0700

    Added login template, route, controller

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require("../models/tourModel");
+// const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -15,7 +16,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour (includes reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: "reviews",
@@ -40,3 +41,9 @@ exports.getTour = catchAsync(async (req, res) => {
       tour,
     });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render("login", {
+    title: "Log into your account",
+  });
+};
