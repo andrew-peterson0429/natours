f770f0789abee4e83af47e35bdc58881ae591a1a
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2024-01-07 22:29:24 -0700

    Connected user to template

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const viewsController = require("../controllers/viewsController");
 const authController = require("../controllers/authController");
 
-router.get("/", viewsController.getOverview);
-router.get("/tour/:slug", authController.protect, viewsController.getTour);
+router.use(authController.isLoggedIn);
 
+router.get("/", viewsController.getOverview);
+router.get("/tour/:slug", authController.isLoggedIn, viewsController.getTour);
 router.get("/login", viewsController.getLoginForm);
 
 module.exports = router;
