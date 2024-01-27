8bf1f5725c8ca1b67a5a113f3912ec02c539d1c9
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2024-01-26 21:41:17 -0700

    Added middleware for processing tour images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,10 +25,36 @@ exports.uploadTourImages = upload.fields([
   { name: "images", maxCount: 3 }
 ]);
 
-exports.resizeTourImages = (req, res, next) => {
-  console.log("this is req.files", req.files);
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Other images
+  req.body.images = [];
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const fileName = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${fileName}`);
+
+      req.body.images.push(fileName);
+    })
+  );
+
   next();
-};
+});
 
 // This is a middleware
 exports.aliasTopTours = (req, res, next) => {
