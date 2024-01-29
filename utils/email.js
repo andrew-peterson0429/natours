a11d7fe4271a519100d40bd2b0ed4a8f603dab5a
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2024-01-28 21:04:30 -0700

    Created email template, added welcome email to middleware

--- utils/email.js
+++ utils/email.js
@@ -3,19 +3,24 @@ const pug = require("pug");
 const htmlToText = require("html-to-text");
 
 // new Email(user, url).sendWelcome();
-
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
     this.url = url;
-    this.from = `Andrew Peterson <${process.env.EMAIL_FROM}>`;
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
-    if (process.env.NodeEnv === "production") {
+    if (process.env.NODE_ENV === "production") {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: "SendGrid",
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     }
 
     return nodemailer.createTransport({
@@ -50,10 +55,17 @@ module.exports = class Email {
     };
 
     // 3) Create a transport and send email
-    await this.newTransport.sendMail(mailOptions);
+    await this.newTransport().sendMail(mailOptions);
   }
 
   async sendWelcome() {
     await this.send("welcome", "Welcome to the Natours Family!");
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
   }
 };
