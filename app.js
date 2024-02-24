17bbbf924f71da5514c0474e2cf0ad950c470f6c
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2024-02-23 21:27:33 -0700

    Added cors options to all routes

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.set("views", path.join(__dirname, "views"));
 // Implement CORS
 app.use(cors());
 
+app.options("*", cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, "public")));
 
